2ab0fa1a2ccc48c38d588541c870322ff03556f7
Guilherme Almeida <guica.almeida@gmail.com>
2024-01-12 20:20:56 -0300

    fix: add correct font

--- src/fonts.ts
+++ src/fonts.ts
@@ -4,71 +4,16 @@ import localFont from "next/font/local";
 export const timesNow = localFont({
   src: [
     {
-      path: "../public/fonts/TimesNow-BoldItalic.ttf",
-      weight: "700",
-      style: "italic",
-    },
-    {
-      path: "../public/fonts/TimesNow-Bold.ttf",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/TimesNow-ExtraBold.ttf",
-      weight: "800",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/TimesNow-ExtraBoldItalic.ttf",
-      weight: "800",
-      style: "italic",
-    },
-    {
-      path: "../public/fonts/TimesNow-ExtraLightItalic.ttf",
+      path: "../public/fonts/AGA_Times_Now_Regular.woff",
       weight: "200",
-      style: "italic",
-    },
-    {
-      path: "../public/fonts/TimesNow-ExtraLight.ttf",
-      weight: "200",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/TimesNow-LightItalic.ttf",
-      weight: "300",
-      style: "italic",
-    },
-    {
-      path: "../public/fonts/TimesNow-Light.ttf",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/TimesNow-SemiBoldItalic.ttf",
-      weight: "600",
-      style: "italic",
-    },
-    {
-      path: "../public/fonts/TimesNow-SemiBold.ttf",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/TimesNow-SemiLightItalic.ttf",
-      weight: "400",
-      style: "italic",
-    },
-    {
-      path: "../public/fonts/TimesNow-SemiLight.ttf",
-      weight: "400",
       style: "normal",
     },
   ],
-  variable: '--font-timesNow',
+  variable: "--font-timesNow",
 });
 
 export const azeret_mono = Azeret_Mono({
   subsets: ["latin"],
   display: "swap",
-  variable: "--font-azaretMono"
+  variable: "--font-azaretMono",
 });
