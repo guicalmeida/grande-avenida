da6e511a31f66b80c052f3a267bf5a3c0ac53c1c
Guilherme Almeida <guica.almeida@gmail.com>
2024-02-25 13:11:18 -0300

    fix: dont show date is course is finished

--- src/components/CourseInfo.tsx
+++ src/components/CourseInfo.tsx
@@ -1,7 +1,7 @@
 import { Course, availabilityMap } from "@/models/course.model";
 import dayjs from "dayjs";
 import LinkButton from "./LinkButton";
-dayjs.locale('pt-br')
+dayjs.locale("pt-br");
 
 export default function CourseInfo({
   course,
@@ -25,16 +25,16 @@ export default function CourseInfo({
     <div className="flex items-center md:justify-between md:flex-col md:gap-7 font-azeret font-medium uppercase text-lg md:grow md:items-start md:h-full">
       <div className="flex md:gap-7 items-center">
         <p className="h-fit mr-4 md:mr-0 font-medium text-xs md:text-lg md:leading-[22px] uppercase whitespace-pre">
-          {isPastCourse ? 'curso\nfinalizado' : status}
+          {isPastCourse ? "curso finalizado" : status}
         </p>
-        <p className="block bg-primary px-[18px] md:px-[30px] py-3 md:py-[20px] font-azeret font-medium uppercase leading-[9px] md:leading-[14px] text-[9px] md:text-sm border-solid border-[1px] md:border-2 border-black rounded-[100px] w-[fit-content] mr-5">
-          {datesStr}
-        </p>
+        {!isPastCourse && (
+          <p className="block bg-primary px-[18px] md:px-[30px] py-3 md:py-[20px] font-azeret font-medium uppercase leading-[9px] md:leading-[14px] text-[9px] md:text-sm border-solid border-[1px] md:border-2 border-black rounded-[100px] w-[fit-content] mr-5">
+            {datesStr}
+          </p>
+        )}
       </div>
       {moreDetails && (
-        <LinkButton href={`cursos/${slug}`}>
-          Saiba mais
-        </LinkButton>
+        <LinkButton href={`cursos/${slug}`}>Saiba mais</LinkButton>
       )}
     </div>
   );
