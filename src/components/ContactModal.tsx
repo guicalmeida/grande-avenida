fe67a59b358a252666291d065cd3d7a31f764f89
Guilherme Almeida <guica.almeida@gmail.com>
2024-02-26 09:39:25 -0300

    feat: create mobile modal

--- src/components/ContactModal.tsx
+++ src/components/ContactModal.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { Dispatch } from "react";
 import ContactForm from "./ContactForm";
@@ -12,13 +12,13 @@ export default function ContactModal({
 }) {
   return (
     <div
-      className="bg-[#00000080] w-screen h-screen fixed top-0 left-0 overflow-hidden flex items-center justify-center cursor-zoom-out"
+      className="bg-[#00000080] w-screen h-screen fixed top-0 left-0 overflow-hidden flex items-start md:items-center justify-center cursor-zoom-out z-40"
       onClick={() => {
         setIsOpen(false);
       }}
     >
       <div
-        className="bg-white rounded-xl min-w-[500px] min-h-[400px] cursor-auto"
+        className="bg-white rounded-xl  w-full md:w-[500px] h-[400px] cursor-auto pointer-events-none mt-20 md:mt-0 mx-8 md:mx-0"
         onClick={(e) => {
           e.stopPropagation();
         }}
