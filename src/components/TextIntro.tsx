3d66909ef5fa5be4d7bbd176077358ba30aba39f
Guilherme Almeida <guica.almeida@gmail.com>
2024-03-31 16:29:10 -0300

    feat: add new texts

--- src/components/TextIntro.tsx
+++ src/components/TextIntro.tsx
@@ -2,19 +2,18 @@ export default function TextIntro() {
   return (
     <div>
       <h1 className="md:w-[75%] font-timesNow mb-9 md:mb-20 text-[40px] md:text-[120px] leading-[40px] md:leading-[120px] md:tracking-[-2.4px]">
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed.
+        Poesia é pão de forma.
       </h1>
       <p className="md:ml-auto md:max-w-[576px] max-w-[290px] md:w-[575px] mb-5 md:mb-32 font-azeret text-xs md:text-[18px] font-medium md:leading-[26px]">
-        A Grande Avenida se revela não apenas como uma extensão urbana, mas como
-        um vasto campo educativo e criativo, onde as ruas se transformam em
-        salas de aula, oferecendo um espaço dinâmico para cursos que inspiram e
-        conectam comunidades.
+        A Grande Avenida mantém programação de cursos, pequenas instalações e
+        encontros. Estamos na República e juntamos pessoas interessadas no
+        avesso da história das mídias e da arte contemporânea.
       </p>
       <p className="ml-auto md:ml-0 md:max-w-[576px] max-w-[290px] md:w-[575px] font-azeret text-xs md:text-[18px] font-medium md:leading-[26px]">
-        A Grande Avenida se revela não apenas como uma extensão urbana, mas como
-        um vasto campo educativo e criativo, onde as ruas se transformam em
-        salas de aula, oferecendo um espaço dinâmico para cursos que inspiram e
-        conectam comunidades.
+        Os cursos da Avenida procuram aproximar movimentos contemporâneos no uso
+        das mídias com a história da arte e da antiarte. Nossa curiosidade gira
+        em torno de 3 eixos principais: cultura digital, ruído e estética
+        glitter, e media art.
       </p>
     </div>
   );
