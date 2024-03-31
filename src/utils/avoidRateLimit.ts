4be6b1dbb1c83c09fa61b2669283932c23ca065e
Guilherme Almeida <guica.almeida@gmail.com>
2024-03-31 16:57:29 -0300

    fix: only throttle on build

--- src/utils/avoidRateLimit.ts
+++ src/utils/avoidRateLimit.ts
@@ -1,4 +1,9 @@
 export function avoidRateLimit(delay = 2000) {
+  if (!(process.env.IS_BUILD === "true")) {
+    return new Promise((resolve) => {
+      resolve("");
+    });
+  }
   return new Promise((resolve) => {
     setTimeout(resolve, delay);
   });
